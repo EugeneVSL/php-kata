155127331239bc756075dbf281717d98711422f8
Eugene <eugenevasile@gmail.com>
2023-07-11 18:44:52 +0200

    Switched to using PhpUnit Data Provider
    Switched to using PhpUnit Data Provider

--- tests/PrimeFactorsTest.php
+++ tests/PrimeFactorsTest.php
@@ -7,50 +7,24 @@ class PrimeFactorsTest extends TestCase
 {
 
     /**
-     * 
      * @test
+     * @dataProvider factors 
      */
-    function generates_prime_factors_for_1()
+    function generates_prime_factors_for_1($number, $expected)
     {
 
         $factors = new PrimeFactors;
 
-        $this->assertEquals([], $factors->generate(1));
+        $this->assertEquals($expected, $factors->generate($number));
     }
 
-    /**
-     * 
-     * @test
-     */
-    function generates_prime_factors_for_2()
+    public static function factors()
     {
-
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([2], $factors->generate(2));
-    }
-
-    /**
-     * 
-     * @test
-     */
-    function generates_prime_factors_for_3()
-    {
-
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([3], $factors->generate(3));
-    }
-
-    /**
-     * 
-     * @test
-     */
-    function generates_prime_factors_for_4()
-    {
-
-        $factors = new PrimeFactors;
-
-        $this->assertEquals([2, 2], $factors->generate(4));
-    }
+        return [
+            [1, []],
+            [2, [2]],
+            [3, [3]],
+            [4, [2, 2]],
+        ];
+    } 
 }
